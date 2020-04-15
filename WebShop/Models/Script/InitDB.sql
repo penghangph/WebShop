07b02d958e3a6d3afd6f4674b11a2ddc10e3f8f5
PengHang <penghangph@163.com>
2020-04-15 17:32:58 +0800

    添加sql语句

--- WebShop/Models/Script/InitDB.sql
+++ WebShop/Models/Script/InitDB.sql
@@ -1,22 +1,24 @@
-
+--产品分类表
 insert into ProCateSet values('家用电器',null)
 insert into ProCateSet values('电脑/办公',null)
 insert into ProCateSet values('家装/厨具',null)
 insert into ProCateSet values('服装',null)
 
-insert into ProCateSet values('电视',1)
-insert into ProCateSet values('空调',1)
-insert into ProCateSet values('洗衣机',1)
-insert into ProCateSet values('冰箱',1)
+insert into ProductCates values('电视',1)
+insert into ProductCates values('空调',1)
+insert into ProductCates values('洗衣机',1)
+insert into ProductCates values('冰箱',1)
 
-insert into ProCateSet values('整机',2)
-insert into ProCateSet values('配件',2)
-insert into ProCateSet values('外设',2)
-insert into ProCateSet values('耗材',2)
+insert into ProductCates values('整机',2)
+insert into ProductCates values('配件',2)
+insert into ProductCates values('外设',2)
+insert into ProductCates values('耗材',2)
 
 
-select * from procateset
+select * from ProductCates
 
-insert into ProductSet values('小米电视4X 55英寸 4K超高清 HDR 蓝牙语音遥控 2GB+8GB 教育电视 人工智能语音网络液晶平板电视 L55M5-AD',5,1799.00,1599.00,'商品名称：小米L55M5-AD商品编号：7641991商品毛重：17.0kg商品产地：中国大陆','2020-4-15 15:04',999,'/image/tv/1.jpg',)
+
+--产品表
+insert into Products values('小米电视4X 55英寸 4K超高清 HDR 蓝牙语音遥控 2GB+8GB 教育电视 人工智能语音网络液晶平板电视 L55M5-AD',5,1799.00,1599.00,'商品名称：小米L55M5-AD商品编号：7641991商品毛重：17.0kg商品产地：中国大陆','2020-4-15 15:04',999,'/image/tv/1.jpg')
 
 
